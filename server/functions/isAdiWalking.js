6546a3dde64d126516f5bc9bcf4adb40644d71fd
Adi Dahiya <adi.dahiya14@gmail.com>
2019-02-13 14:30:24 -0500

    actually commit the lambda functions

---
+++ server/functions/isAdiWalking.js
@@ -0,0 +1,32 @@
+import request from "request";
+
+/**
+ * Tells you whether Adi is on a walk right now.
+ */
+export function handler(event, context, callback) {
+    const { foursquare_client_id, foursquare_client_secret } = process.env;
+
+    if (foursquare_client_id == null || foursquare_client_secret == null) {
+        callback(new Error("Could not find foursquare API keys"));
+        return;
+    }
+
+    request({
+        uri: "https://api.foursquare.com/v2/users/self",
+        method: "GET",
+        qs: {
+            client_id: foursquare_client_id,
+            client_secret: foursquare_client_secret,
+            v: "201902013",
+        },
+    }, (error, response, body) => {
+        if (error != null) {
+            callback(error);
+        } else {
+            callback(null, {
+                statusCode: 200,
+                body,
+            })
+        }
+    });
+};
---
+++ server/functions/sendAdiMessage.js
@@ -0,0 +1,29 @@
+import twilio from "twilio";
+
+/**
+ * Sends Adi a message.
+ * Expects request body { message: string }
+ */
+export function handler(event, context, callback) {
+    const { twilio_client_id, twilio_client_secret } = process.env;
+
+    if (twilio_client_id == null || twilio_client_secret == null) {
+        callback(new Error("Could not find twilio API keys"));
+        return;
+    }
+
+    const { message } = JSON.parse(event.body);
+
+    const client = new twilio(twilio_client_id, twilio_client_secret);
+    client.messages.create({
+        body: message,
+        to: "+19082940182",
+        from: "+19082937178",
+    }).then((responseMessage) => {
+        console.log(responseMessage.sid);
+        callback(null, {
+            statusCode: 200,
+            body: responseMessage.sid,
+        });
+    });
+}
